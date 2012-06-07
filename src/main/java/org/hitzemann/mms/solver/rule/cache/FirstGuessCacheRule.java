c93ea097cbc6227e9d0956b297bb3039d78e69d9
Christian Schuster <christian.schuster@s2000.tu-chemnitz.de>
2012-06-07 17:53:35 +0200

    added rule implementation with a cache for the first guess

---
+++ src/main/java/org/hitzemann/mms/solver/rule/cache/FirstGuessCacheRule.java
@@ -0,0 +1,72 @@
+package org.hitzemann.mms.solver.rule.cache;
+
+import java.util.Collection;
+
+import org.hitzemann.mms.model.ErgebnisKombination;
+import org.hitzemann.mms.model.SpielKombination;
+import org.hitzemann.mms.solver.rule.IRule;
+
+/**
+ * <p>
+ * Eine Regel, die den von einer anderen Regel zurückgegebenen Zug speichert.
+ * </p>
+ * 
+ * <p>
+ * Damit sich der Cache für den Aufrufer neutral verhält, muss die von der
+ * gekapselten Regel bei {@link IRule#getGuess(Collection)} zurückgegebene
+ * {@link SpielKombination} konstant sein.
+ * </p>
+ * 
+ * @author chschu
+ */
+public final class FirstGuessCacheRule implements IRule {
+
+	/**
+	 * Die gekapselte Regel.
+	 */
+	private final IRule delegate;
+
+	/**
+	 * Der zu verwendende Zug.
+	 */
+	private SpielKombination guess;
+
+	/**
+	 * Erzeugt eine Instanz ohne vorgegebenen Zug.
+	 * 
+	 * @param theDelegate
+	 *            Die gekapselte Regel.
+	 */
+	public FirstGuessCacheRule(final IRule theDelegate) {
+		this(theDelegate, null);
+	}
+
+	/**
+	 * Erzeugt eine Instanz mit vorgegebenem Zug.
+	 * 
+	 * @param theDelegate
+	 *            Die gekapselte Regel.
+	 * @param theGuess
+	 *            Der zu verwendende Zug, oder <code>null</code> falls er
+	 *            automatisch ermittelt werden soll.
+	 */
+	public FirstGuessCacheRule(final IRule theDelegate,
+			final SpielKombination theGuess) {
+		delegate = theDelegate;
+		guess = theGuess;
+	}
+
+	@Override
+	public SpielKombination getGuess(
+			final Collection<SpielKombination> candidates) {
+		if (guess == null) {
+			guess = delegate.getGuess(candidates);
+		}
+		return guess;
+	}
+
+	@Override
+	public IRule getRuleForResponse(final ErgebnisKombination response) {
+		return delegate.getRuleForResponse(response);
+	}
+}
---
+++ src/test/java/org/hitzemann/mms/solver/rule/cache/FirstGuessCacheRuleTest.java
@@ -0,0 +1,111 @@
+package org.hitzemann.mms.solver.rule.cache;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyCollectionOf;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hitzemann.mms.model.ErgebnisKombination;
+import org.hitzemann.mms.model.SpielKombination;
+import org.hitzemann.mms.solver.rule.IRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Tests for {@link FirstGuessCacheRule}.
+ * 
+ * @author chschu
+ */
+@RunWith(MockitoJUnitRunner.class)
+public final class FirstGuessCacheRuleTest {
+
+	/**
+	 * Mock für die zu übergebende {@link IRule}.
+	 */
+	@Mock
+	private IRule ruleMock;
+
+	/**
+	 * Test für {@link IRule#getGuess(java.util.Collection)} bei einem
+	 * "Cache Miss".
+	 */
+	@Test
+	public void testGetGuessCacheMiss() {
+		final FirstGuessCacheRule underTest = new FirstGuessCacheRule(ruleMock);
+		final List<SpielKombination> candidates = Collections.emptyList();
+		final SpielKombination guess = new SpielKombination(1);
+
+		when(ruleMock.getGuess(anyCollectionOf(SpielKombination.class)))
+				.thenReturn(guess);
+
+		assertSame(guess, underTest.getGuess(candidates));
+
+		verify(ruleMock).getGuess(same(candidates));
+		verifyNoMoreInteractions(ruleMock);
+	}
+
+	/**
+	 * Test für {@link IRule#getGuess(java.util.Collection)} bei einem
+	 * "Cache Hit".
+	 */
+	@Test
+	public void testGetGuessCacheHit() {
+		final SpielKombination guess = new SpielKombination(1);
+
+		final FirstGuessCacheRule underTest = new FirstGuessCacheRule(ruleMock,
+				guess);
+		final List<SpielKombination> candidates = Collections.emptyList();
+
+		assertSame(guess, underTest.getGuess(candidates));
+
+		verifyNoMoreInteractions(ruleMock);
+	}
+
+	/**
+	 * Test für {@link IRule#getGuess(java.util.Collection)} bei einem
+	 * "Cache Hit" im zweiten Anlauf.
+	 */
+	@Test
+	public void testGetGuessCacheHitSecondCall() {
+		final FirstGuessCacheRule underTest = new FirstGuessCacheRule(ruleMock);
+		final List<SpielKombination> candidates = Collections.emptyList();
+		final SpielKombination guess = new SpielKombination(1);
+
+		when(ruleMock.getGuess(anyCollectionOf(SpielKombination.class)))
+				.thenReturn(guess);
+
+		// zweimal aufrufen (ruleMock wird nur einmal aufgerufen)
+		assertSame(guess, underTest.getGuess(candidates));
+		assertSame(guess, underTest.getGuess(candidates));
+
+		verify(ruleMock).getGuess(same(candidates));
+		verifyNoMoreInteractions(ruleMock);
+	}
+
+	/**
+	 * Test für {@link IRule#getRuleForResponse(ErgebnisKombination)}.
+	 */
+	@Test
+	public void testGetRuleForResponse() {
+		final FirstGuessCacheRule underTest = new FirstGuessCacheRule(ruleMock);
+		final IRule nextRuleMock = mock(IRule.class);
+		final ErgebnisKombination response = new ErgebnisKombination(0, 0);
+
+		when(ruleMock.getRuleForResponse(any(ErgebnisKombination.class)))
+				.thenReturn(nextRuleMock);
+
+		assertSame(nextRuleMock, underTest.getRuleForResponse(response));
+
+		verify(ruleMock).getRuleForResponse(same(response));
+		verifyNoMoreInteractions(ruleMock, nextRuleMock);
+	}
+}
