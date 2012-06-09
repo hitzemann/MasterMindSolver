15f49a41e8a7b06d6611e2acf410bfcce815d31d
Simon Hitzemann <simon@hitzemann.org>
2012-06-09 20:52:40 +0200

    org.hitzemann.mms.model.DefaultSpielKombinationFactoryTest.VariablePins gefixt

--- src/main/java/org/hitzemann/mms/model/DefaultSpielKombinationFactory.java
+++ src/main/java/org/hitzemann/mms/model/DefaultSpielKombinationFactory.java
@@ -1,5 +1,6 @@
 package org.hitzemann.mms.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +14,40 @@ public final class DefaultSpielKombinationFactory implements
 	@Override
 	public List<SpielKombination> erzeugeAlle(final int pins) {
 		// TODO muss noch implementiert werden
-		throw new UnsupportedOperationException("TODO");
+		//throw new UnsupportedOperationException("TODO");
+		final List<SpielKombination> alleErzeugten = new ArrayList<SpielKombination>();
+		erzeugeAlleKombinationenRekursiv(new SpielStein[pins], 0, alleErzeugten);
+		return alleErzeugten;
+	}
+	
+	/**
+	 * Rekursiver Algorithmus zur Erzeugung aller {@link SpielKombination}en.
+	 * 
+	 * @param spielSteine
+	 *            Das rekursiv zu füllende Array mit {@link SpielStein}en. Die
+	 *            Länge dieses Arrays bestimmt die Länge der erzeugten
+	 *            Kombinationen.
+	 * @param iterierOffset
+	 *            Das in diesem Aufruf zu iterierende Offset in das
+	 *            spielSteine-Array. Alle höheren Indizes werden durch rekursive
+	 *            Aufrufe dieser Methode befüllt.
+	 * @param ergebnisList
+	 *            Die {@link List} zum Einsammeln der erzeugten
+	 *            {@link SpielKombination}en.
+	 */
+	private void erzeugeAlleKombinationenRekursiv(
+			final SpielStein[] spielSteine, final int iterierOffset,
+			final List<SpielKombination> ergebnisList) {
+		if (iterierOffset < spielSteine.length) {
+			for (SpielStein spielStein : SpielStein.values()) {
+				spielSteine[iterierOffset] = spielStein;
+				erzeugeAlleKombinationenRekursiv(spielSteine,
+						iterierOffset + 1, ergebnisList);
+			}
+		} else {
+			// Array klonen, damit nicht alle erzeugten Kombinationen das
+			// gleiche benutzen
+			ergebnisList.add(new SpielKombination(spielSteine.clone()));
+		}
 	}
 }
