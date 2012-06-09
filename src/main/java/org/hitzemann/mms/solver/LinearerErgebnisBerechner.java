d257d19d1412094e9ebb0cdcdde86ea652b3ccb0
Christian Schuster <christian.schuster@s2000.tu-chemnitz.de>
2012-06-10 00:07:29 +0200

    kleine Optimierung: "schwarzProFarbe" wird nicht benötigt

--- src/main/java/org/hitzemann/mms/solver/LinearerErgebnisBerechner.java
+++ src/main/java/org/hitzemann/mms/solver/LinearerErgebnisBerechner.java
@@ -5,15 +5,15 @@ import org.hitzemann.mms.model.SpielKombination;
 import org.hitzemann.mms.model.SpielStein;
 
 /**
- * Implementierung von {@link IErgebnisBerechnung} mit linearer Laufzeit.
+ * Implementierung von {@link IErgebnisBerechnung} mit Laufzeit in O(Anzahl Farben + Länge der Kombination).
  * 
  * <p>
  * Das Ergebnis wird folgendermaßen bestimmt:
  * </p>
  * 
  * <ul>
- * <li>schwarz := Summe(schwarzProFarbe[X], X über alle Farben}</li>
- * <li>weiss := Summe(Minimum(geheimProFarbe[X], geratenProFarbe[X]) - schwarzProFarbe[X], X über alle Farben}</li>
+ * <li>schwarz := Anzahl der vollständig korrekt geratenen Spielsteine</li>
+ * <li>weiss := Summe(Minimum(geheimProFarbe[X], geratenProFarbe[X]), X über alle Farben) - schwarz</li>
  * </ul>
  * 
  * <p>
@@ -21,7 +21,6 @@ import org.hitzemann.mms.model.SpielStein;
  * </p>
  * 
  * <ul>
- * <li>schwarzProFarbe[X] := Anzahl der vollständig korrekt geratenen Spielsteine mit der Farbe X</li>
  * <li>geheimProFarbe[X] := Anzahl der Spielsteine mit der Farbe X in der geheimen Kombination</li>
  * <li>geratenProFarbe[X] := Anzahl der Spielsteine mit der Farbe X in der geratenen Kombination</li>
  * </ul>
@@ -30,44 +29,42 @@ import org.hitzemann.mms.model.SpielStein;
  */
 public final class LinearerErgebnisBerechner implements IErgebnisBerechnung {
 
-    @Override
-    public ErgebnisKombination berechneErgebnis(final SpielKombination geheim, final SpielKombination geraten) {
-        if (geheim == null) {
-            throw new IllegalArgumentException("Geheime Kombination ist null!");
-        }
-        if (geraten == null) {
-            throw new IllegalArgumentException("Geratene Kombination ist null!");
-        }
+	@Override
+	public ErgebnisKombination berechneErgebnis(final SpielKombination geheim, final SpielKombination geraten) {
+		if (geheim == null) {
+			throw new IllegalArgumentException("Geheime Kombination ist null!");
+		}
+		if (geraten == null) {
+			throw new IllegalArgumentException("Geratene Kombination ist null!");
+		}
 
-        final int groesse = geheim.getSpielSteineCount();
-        if (geraten.getSpielSteineCount() != groesse) {
-            throw new IllegalArgumentException("Spielkombinationen haben unterschiedliche Größen!");
-        }
+		final int groesse = geheim.getSpielSteineCount();
+		if (geraten.getSpielSteineCount() != groesse) {
+			throw new IllegalArgumentException("Spielkombinationen haben unterschiedliche Größen!");
+		}
 
-        final int anzahlFarben = SpielStein.values().length;
+		final int anzahlFarben = SpielStein.values().length;
 
-        // benötigte Teilwerte ermitteln (pro Farbe zählen)
-        final int[] schwarzProFarbe = new int[anzahlFarben];
-        final int[] geheimProFarbe = new int[anzahlFarben];
-        final int[] geratenProFarbe = new int[anzahlFarben];
-        for (int i = 0; i < groesse; i++) {
-            final int geheimFarbe = geheim.getSpielStein(i).ordinal();
-            final int geratenFarbe = geraten.getSpielStein(i).ordinal();
-            geheimProFarbe[geheimFarbe]++;
-            geratenProFarbe[geratenFarbe]++;
-            if (geheimFarbe == geratenFarbe) {
-                schwarzProFarbe[geheimFarbe]++;
-            }
-        }
+		// "schwarz", "geheimProFarbe[X]" und "geratenProFarbe[X]" berechnen
+		int schwarz = 0;
+		final int[] geheimProFarbe = new int[anzahlFarben];
+		final int[] geratenProFarbe = new int[anzahlFarben];
+		for (int i = 0; i < groesse; i++) {
+			final int geheimFarbe = geheim.getSpielStein(i).ordinal();
+			final int geratenFarbe = geraten.getSpielStein(i).ordinal();
+			geheimProFarbe[geheimFarbe]++;
+			geratenProFarbe[geratenFarbe]++;
+			if (geheimFarbe == geratenFarbe) {
+				schwarz++;
+			}
+		}
 
-        // Summen bilden (über alle Farben)
-        int schwarz = 0;
-        int weiss = 0;
-        for (int i = 0; i < anzahlFarben; i++) {
-            schwarz += schwarzProFarbe[i];
-            weiss += Math.min(geheimProFarbe[i], geratenProFarbe[i]) - schwarzProFarbe[i];
-        }
+		// "weiss" berechnen
+		int weiss = -schwarz;
+		for (int i = 0; i < anzahlFarben; i++) {
+			weiss += Math.min(geheimProFarbe[i], geratenProFarbe[i]);
+		}
 
-        return new ErgebnisKombination(schwarz, weiss);
-    }
+		return new ErgebnisKombination(schwarz, weiss);
+	}
 }
